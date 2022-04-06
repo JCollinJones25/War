2100d71d7d98bfbfb4348c902aa6d5f70e0e5328
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-04-06 17:18:54 -0400

    deck 1 is successfully shuffled

--- scripts/app.js
+++ scripts/app.js
@@ -12,10 +12,6 @@ class Cards {
 Cards.suit = ['hearts', 'spades', 'clubs', 'diamonds']
 Cards.rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 Cards.score = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-// console.log(Cards.suit)
-
-const card1 = new Cards('spades', 'ace', 1)
-// console.log(card1)
 
 class Deck {
     constructor(length = 52) {
@@ -33,18 +29,35 @@ class Deck {
 }
 const deck1 = new Deck()
 deck1.draw()
-
-
-// console.log(Cards.suit)
-// console.log(Cards.rank)
-// console.log(Cards.score)
-// console.log(Deck.cards)
 console.log(deck1)
+
+const shuffledDeck = deck1.cards.sort((a, b) => .5 - Math.random())
+console.log(shuffledDeck)
+
+// create a new deck based off deck class that is deck1 but shuffled
+// const shuffledDeck = new Deck()
+// function shuffle(){
+//     const randomCard = deck1[Math.floor(Math.random() * i)]
+//     deck1.splice(randomCard)
+//     this.cards.push(randomCard)
+    
+// }
+// shuffledDeck.draw()
 
 
 // shuffle the deck and divide by two
-function shuffle () {
-    for (let i = 0; i < deck1.length; i++) {
-        let j = Math.floor(Math.random() * i)
-    }
-}
+
+// let shuffledDeck = []
+
+// function shuffle() {
+// }
+//     for (let i = 0; i <= deck1.length; i++) {
+//     }
+
+
+// create a new array
+// make a for loop that iterates over the length of the unshuffled array
+// Get a random number up to the length of the array and round down
+// Splice that random number out of your old array
+// Push the spliced element to your new array
+// Outside the loop, have the function return your new array
