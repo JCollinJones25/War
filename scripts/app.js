97cd5630fa9318628259962e8b215035578c588e
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-04-06 15:52:54 -0400

    restart button added

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
 // create an object of a deck of cards
 // object includes card rank, suit, and score
+// method below taken from own work done earlier in course on card game lab
 
 class Cards {
-    constructor(suit, rank, score){
+    constructor(suit, rank, score) {
         this.suit = suit
         this.rank = rank
         this.score = score
@@ -10,30 +11,26 @@ class Cards {
 }
 Cards.suit = ['hearts', 'spades', 'clubs', 'diamonds']
 Cards.rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
-Cards.score = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+Cards.score = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 // console.log(Cards.suit)
 
 const card1 = new Cards('spades', 'ace', 1)
 // console.log(card1)
 
 class Deck {
-    constructor(length = 52){
+    constructor(length = 52) {
         this.length = length
         this.cards = []
     }
     draw() {
-        for(let i = 0; i < Cards.suit.length; i++){
-            for(let j = 0; j < Cards.rank.length; j++){
-                for(let k = 0; k < Cards.rank.length; k++){
-                const newCard = new Cards(Cards.suit[i], Cards.rank[j], Cards.score[k])
+        for (let i = 0; i < Cards.suit.length; i++) {
+            for (let j = 0; j < Cards.rank.length; j++) {
+                const newCard = new Cards(Cards.suit[i], Cards.rank[j], Cards.score[j])
                 this.cards.push(newCard)
-            }
             }
         }
     }
 }
-
-
 const deck1 = new Deck()
 deck1.draw()
 
@@ -42,4 +39,12 @@ deck1.draw()
 // console.log(Cards.rank)
 // console.log(Cards.score)
 // console.log(Deck.cards)
-// console.log(deck1)
+console.log(deck1)
+
+
+// shuffle the deck and divide by two
+function shuffle () {
+    for (let i = 0; i < deck1.length; i++) {
+        let j = Math.floor(Math.random() * i)
+    }
+}
